2f105f079618825e705b2b2c3fb4eff25ac851a8
Augustine Dunn <wadunn83@gmail.com>
2010-05-15 17:57:19 -0700

    changed align.py

---
+++ rSeq/utils/align.py
@@ -0,0 +1,9 @@
+from rSeq.utils.file_io import ProgramRun
+
+
+        
+def bowtieMakeIndex(optionsHere):
+    """Create bowtie indexes from new fasta set."""
+    
+def bowtieAlign(optionsHere):
+    """Run alignment of fastQ to bowtie index."""
