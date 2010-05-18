80a8ce8a360e058e3d11500609c4d1e602a5327e
Augustine Dunn <wadunn83@gmail.com>
2010-05-17 21:36:30 -0700

    removed file_io.py in favor of files.py

---
+++ rSeq/utils/files.py
@@ -0,0 +1,51 @@
+import sys
+from rSeq.utils.errors import *
+
+
+def onlyInA(fileA,fileB,outFile):
+    """Takes two files. Writes a third file with the lines that are unique to
+    fileA.  NOTE: Can be Memory intensive for large files."""
+    
+    fileA = open(fileA,'rU')
+    fileB = open(fileB,'rU')
+    
+    lineDict = {}
+    
+    # Get line info
+    for line in fileA:
+        line = line.strip('\n')
+        if line in lineDict:
+            lineDict[line][0] += 1
+        else:
+            lineDict[line] = [1,0]
+    
+    for line in fileB:
+        line = line.strip('\n')
+        if line in lineDict:
+            lineDict[line][1] += 1
+        else:
+            lineDict[line] = [0,1]
+    
+    # write lines unique to fileA
+    outFile = open(outFile,'w')
+    rLines = 0
+    for item in lineDict.iteritems():
+        line   = item[0]
+        counts = item[1]
+        if counts[1] == 0:
+            if counts[0] == 1:
+                outFile.write('%s\n' % (line))
+            elif counts[0] > 1:
+                rLines += 1
+                outFile.write('%s\n' % (line))
+            elif counts[0] == 0:
+                raise UnexpectedValueError('ERROR: it looks like line "%s" does not occur in either file!!\n"lineDict" may have been corrupted.' % \
+                                           (line))
+        else:
+            continue    
+    if rLines:
+        sys.stderr.write('WARNING: %s lines written to %s occured more than once in %s, but %s is now non-redundant.' %\
+                         (rLines,outFile.name,fileA.name,outFile.name))
+    outFile.close()
+        
+        
