649d7f2bf235c0755838dd0163a6915757d5f245
Augustine Dunn <wadunn83@gmail.com>
2010-07-12 12:29:19 -0700

    align: -changed exonerateCigar2BEDline(), -added parseEnsemblCigar(), -added parseExonerateCigar(), -added parseCigarString(), -added cigarStr2AlignCoords(), -added samCH2supCntg()

--- rSeq/utils/align.py
+++ rSeq/utils/align.py
@@ -2,6 +2,182 @@ import sys
 from rSeq.utils.errors import *
 from rSeq.utils.externals import runExternalApp
 
+def samCH2supCntg(samFile,outFile,cnvsnDict):
+    from gusPyCode.defs.mosqData.AaCntgCvrt import supContigConvert# FIX THIS!
+    """Convert CHxxx to superContig1.xxx"""
+    
+    outFile = open(outFile, 'w')
+    
+    for line in open(samFile, 'rU'):
+        if line.startswith('@SQ'):
+            line = line.split('\t')
+            line[1] = line[1].split(':')[0],':',cnvsnDict[line[1].split(':')[1]]
+
+def cigarStr2AlignCoords(cigarString,minCoord,maxCoord,alignStrand,cigKind='ensembl',intron='I'):
+    """Returns List of coordinate tuples for each block in form of:
+    [
+    (blkMinCoord_1,blkMaxCoord_1),
+    (blkMinCoord_2,blkMaxCoord_2),
+    ...,
+    (blkMinCoord_n,blkMaxCoord_n)
+    ]
+    
+    NOTE: Assumes 0-referenced "in-between" coordinate system as used in BED/exonserate."""
+    
+    cigInfo = parseCigarString(cigarString,kind=cigKind)
+    
+    # Reverse cigData if alignStrand is neg
+    if alignStrand == ('-' or '-1'):
+        cigInfo.reverse()
+    elif alignStrand == ('+' or '1'):
+        pass
+    else:
+        raise InvalidOptionError("cigarStr2AlignCoords: valid alignStrand values are %s. You gave: %s" \
+                                 % (['-','+','-1','1'],alignStrand))
+    if intron == 'I':
+        ignore = 'D'
+    elif intron == 'D':
+        ignore = 'I'
+    else:
+        raise InvalidOptionError("cigarStr2AlignCoords: valid intron values are %s. You gave: %s" \
+                                 % (['I','D'],intron)) 
+    
+    # 1: remove all 'ignore' info since these seem to relate to insertions in the aligned seq
+    # and do not affect coords on the chrm seq.
+    cigNoIgnores = []
+    for elem in cigInfo:
+        if elem[0] == ignore:
+            continue
+        else:
+            cigNoIgnores.append(elem)
+            
+    # 2: combine any 'M' info that are adjacent to each other after cleaning out the 'ignore' info.
+    cigCmbnMs = []
+    while 1:
+        if not len(cigNoIgnores) > 0:
+            break
+        Ms = []
+        while 1:
+            try:
+                if cigNoIgnores[0][0] == 'M':
+                    Ms.append(cigNoIgnores.pop(0))
+                else:
+                    break
+            except IndexError:
+                break
+        cigCmbnMs.append(('M',sum([x[1] for x in Ms])))
+        if len(cigNoIgnores) > 0:
+            cigCmbnMs.append(cigNoIgnores.pop(0))
+        else:
+            break
+    
+    # 3: walk the cig, keeping track of where on Chrm you are and logging boundaries.
+    blkCoordsRelative = []
+    i = 0
+    while i < len(cigCmbnMs):
+        if cigCmbnMs[i][0] == 'M':
+            end   = sum([x[1] for x in cigCmbnMs[:i+1]]) 
+            start = end - cigCmbnMs[i][1]
+            blkCoordsRelative.append((start,end))
+            i += 1
+        else:
+            i += 1
+    blkCoordsAbsolute = []
+    for coord in blkCoordsRelative:
+        blkCoordsAbsolute.append((coord[0]+minCoord,coord[1]+minCoord))
+    
+    # Confirm that calculated max coord == maxCoord
+    calcMax = max([x[1] for x in blkCoordsAbsolute])
+    if calcMax != maxCoord:
+        raise UnexpectedValueError("cigarStr2AlignCoords: calculated max coord != maxCoord.  My code may not be correct, but check that you used the correct 'intron' value first.")
+    else:
+        return blkCoordsAbsolute
+    
+def parseCigarString(cigarString,kind='ensembl'):
+    """Returns cigarInfo from cigarString in form:
+    [
+    [operationLetter_1,run_1],
+    ...,
+    [operationLetter_n,run_n]
+    ]
+    
+    Validates operation letters before returning."""
+    
+    validKinds  = ['ensembl','exonerate']
+    validCigOps = ['M','D','I']
+    if kind not in validKinds:
+        raise InvalidOptionError('parseCigarString: valid kinds are: %s.  You gave: %s' % (validKinds,kind))
+    if not type(cigarString) == type(''):
+        raise InvalidOptionError('parseCigarString: type(cigarString) != type(""): %s' % (kind))
+    
+    cigData = None
+    if kind == validKinds[0]:
+        cigData = parseEnsemblCigar(cigarString)
+    else:
+        cigData = parseExonerateCigar(cigarString)
+    
+    # Do some validation of cigar operation letters:
+    cigOps = list(set([x[0] for x in cigData]))
+    for op in cigOps:
+        if not op in validCigOps:
+            raise InvalidOptionError("parseCigarString: valid cigar operation letters are %s.  malformed cigarString (%s)." \
+                                     % (validCigOps,cigarString))
+    return cigData
+    
+
+def parseExonerateCigar(cigarString):
+    """Given exonerate style cigar string, return list of lists representing
+    [[letter,number],[letter,number],...]."""
+    cigarList = cigarString.split()
+    
+    # Format cigar info for easy use:
+    cigar = []
+    if not len(cigarList)%2==0:
+        raise InvalidOptionError('parseExonerateCigar: cigarList var is not a list with an even number of indexes.')
+    while cigarList:
+        cigar.append((cigarList.pop(0),int(cigarList.pop(0))))
+    return cigar
+
+def parseEnsemblCigar(cigarString):
+    """Given ensembl style cigar line, return list of lists representing
+    [[letter,number],[letter,number],...]."""
+    def getNumEnd(cigString,index):
+        try:
+            int(cigString[index])
+            index+=1
+            end = getNumEnd(cigString,index)
+        except ValueError:
+            return index
+        return end
+    
+    cigList = []
+    i=0
+    while i < len(cigarString):
+        num = None
+        let = None
+        
+        try:
+            int(cigarString[i])
+            try:
+                end = getNumEnd(cigarString,i)
+                num = int(cigarString[i:end])
+                i = end
+            except ValueError:
+                raise Exception('parseEnsemblCigar.getNumEnd: failed check code.')
+        except ValueError:
+            let = cigarString[i]
+            i+=1
+            
+        if num:
+            let = cigarString[i]
+            i+=1
+            cigList.append((let,num))
+        elif let:
+            cigList.append((let,1))
+        else:
+            raise InvalidOptionError('parseEnsemblCigar: passed through a cycle without assigning let OR num. Check for valid cigarString: %s' % (cigarString))
+    return cigList
+            
 
 def exonerateCigar2BEDline(cigarLine,rgb="0,0,0"):
     """Takes a cigar line from an exonerate output file and returns
@@ -20,14 +196,10 @@ def exonerateCigar2BEDline(cigarLine,rgb="0,0,0"):
     tEnd    = line[7]   # ! will be higher number than tStart if tStrand is '-'
     tStrand = line[8]   #
     score   = line[9]   # "sum of transistion scores used in the dynamic programming."
-    cigInfo = line[10:]  # actual CIGAR encoding
+    cigInfo = ' '.join(line[10:])  # actual CIGAR encoding converted back to string for parser.
     
     # Format cigar info for easy use:
-    cigar = []
-    if not len(cigInfo)%2==0:
-        raise UnexpectedValueError('cigInfo var is not a list with an even number of indexes.')
-    while cigInfo:
-        cigar.append((cigInfo.pop(0),cigInfo.pop(0)))
+    cigar = parseCigarString(cigInfo,type='exonerate')
     if "I" in [x[0] for x in cigar]:
         sys.stderr.write('warn: just encountered a cigar containing "I".  Skipping.\n')
         return '' # for now just do nothing
