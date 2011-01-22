7a6d591a21ce45c760374927f0d5061e1fd74101
Augustine Dunn <wadunn83@gmail.com>
2011-01-22 09:43:02 -0800

    sitRep.py

--- rSeq/utils/sitRep.py
+++ rSeq/utils/sitRep.py
@@ -5,10 +5,32 @@ import sys
 import datetime
 
 
+class StreamHandler(object):
+    """Base class to manage redirected std* streams"""
+    def __init__(self,filePath,runName=None): 
+        """"""
+        self.startTime = datetime.datetime.now().isoformat()
+        self.out = open(filePath,"a")
+    
+    def __del__(self):
+        """Clean-up meathod to make sure that all data is flushed
+        to file and it is closed cleanly."""
+        self.flush()
+        self.close()
+        
+    def write(self):
+        """Dummy method to be over-ridden by children"""
+    def flush(self):
+        """Flush current lines to self.out."""
+        self.out.flush()    
+    def close(self):
+        """Close self.out."""
+        self.out.close()
+        
 class stdOut(object):
     """Mimic a file obj but include useful info in output."""
-    def __init__(self,fileObj): # file should be appendable
-        self.out = fileObj
+    
+        
 
     def write(self,text):
         prefix = datetime.datetime.now().strftime('[%H:%M:%S] ')
