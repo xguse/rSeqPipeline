99e4f42c0ffc64a07ab0361e7e2a836a2399bb15
Augustine Dunn <wadunn83@gmail.com>
2011-01-25 11:55:57 -0800

    utils/errors.py: - tried to standardize/refine error classes

--- rSeq/utils/errors.py
+++ rSeq/utils/errors.py
@@ -1,14 +1,30 @@
 
-class UnexpectedValueError(Exception):
+class UnexpectedValueError(StandardError):
     """When values that "should" not be possible happen; like if a variable was changed unexpectedly."""
     pass
 
-class InvalidFileFormatError(Exception):
+class InvalidFileFormatError(StandardError):
     """When errors occur due to malformed file formats."""
     pass
 
-class InvalidOptionError(Exception):
-    pass
+class MissingArgumentError(StandardError):
+    """When a required argument is missing from the parsed command line options."""
+    def __init__(self,errMsg):
+        self.msg = errMsg
+    def __str__(self):
+        return """ERROR: %s""" % (self.msg)
+
+class InvalidOptionError(StandardError):
+    def __init__(self,optVal,optName,validVals=None):
+        self.optVal    = optVal
+        self.optName   = optName
+        self.validVals = validVals
+        
+    def __str__(self):
+        if self.validVals:
+            return """ERROR: %s is not a valid value for arg:%s.\n\tValid values are: %s""" % (self.optVal,self.optName,self.validVals)
+        else:
+            return """ERROR: %s is not a valid value for arg:%s.""" % (self.optVal,self.optName)
 
 class ExternalError(EnvironmentError):
     """Exception raised when a problem occurs while attempting to run an external system call."""
