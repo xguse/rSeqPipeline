4d5876c1392f00e10b777abd13aa11deb863a944
Augustine Dunn <wadunn83@gmail.com>
2011-02-03 11:51:07 -0800

    utils/expression.py: -added get_feature_reads(pyCgFeat,bams)

--- rSeq/utils/expression.py
+++ rSeq/utils/expression.py
@@ -1,1 +1,13 @@
 from rSeq.utils.errors import *
+import cogent
+import pysam
+
+def get_feature_reads(pyCgFeat,bams):
+    """Given pyCogent feature, return reads (alignments optional) that map to that feature.
+
+    pyCgFeat : pycogent feature object (exon,gene,etc)
+    bams     : list of paths to BAM files (union of files will be used will be use)
+    """
+    raise NotImplementedError()
+def 
+    
