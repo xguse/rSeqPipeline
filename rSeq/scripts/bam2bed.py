638cf70feb35139b443adf0afb0ce8433364a489
Augustine Dunn <wadunn83@gmail.com>
2011-03-14 10:55:38 -0700

    new file: scripts/bam2bed.py: - obtained from http://sourceforge.net/apps/mediawiki/samtools/index.php?title=SAM_protocol#Python_APIs_.28Pysam.29

---
+++ rSeq/scripts/bam2bed.py
@@ -0,0 +1,40 @@
+# obtained from http://sourceforge.net/apps/mediawiki/samtools/index.php?title=SAM_protocol#Python_APIs_.28Pysam.29
+import os, sys, re, optparse
+import pysam
+
+def main( argv = None ):
+    if not argv: argv = sys.argv
+
+    # setup command line parser
+    parser = optparse.OptionParser( version = "%prog version: $Id$", 
+                                    usage = globals()["__doc__"] )
+    parser.add_option("-r", "--region", dest="region", type="string",
+                      help="samtools region string [default=%default]."  )
+    parser.set_defaults( region = None, )
+    (options, args) = parser.parse_args( argv[1:] )
+    if len(args) != 1:
+        raise ValueError( "no samfile specified - see --help for usage" )
+
+    # open BAM and get the iterator
+    samfile = pysam.Samfile( args[0], "rb" )
+    if options.region != None:
+        it = samfile.fetch( region=options.region )
+    else:
+        it = samfile.fetch()
+
+    # calculate the end position and print out BED
+    take = (0, 2, 3) # CIGAR operation (M/match, D/del, N/ref_skip)
+    outfile = sys.stdout
+    for read in it:
+        if read.is_unmapped: continue
+        # compute total length on reference
+        t = sum([ l for op,l in read.cigar if op in take ])
+        if read.is_reverse: strand = "-"
+        else: strand = "+"
+        outfile.write("%s\t%d\t%d\t%s\t%d\t%c\n" %\
+                          ( samfile.getrname( read.rname ),
+                            read.pos, read.pos+t, read.qname,
+                            read.mapq, strand) )            
+
+if __name__ == "__main__":
+    sys.exit( main( sys.argv) )
