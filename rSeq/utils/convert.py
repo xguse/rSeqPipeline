560e567e2c706ce38990dc0cee987b608a29b62a
Augustine Dunn <wadunn83@gmail.com>
2011-04-03 16:25:41 -0700

    utils/convert.py: - open all bedfiles in list for amending

--- rSeq/utils/convert.py
+++ rSeq/utils/convert.py
@@ -21,16 +21,16 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
     bedFile: an open file obj or stdOut obj or a list of open file objs
     region:  samtools region string or list of region strings
     ncpus: num of processors to use if parallel job is run (autodetect -> all avail)
-    
+
     If list is given for bamPath only, all BAMs are written to the same
     bedFile
-    
+
     If list given for bamPath AND bedFile, they must be of equal length,
     and bamPaths[i] is written to bedFile[i].
-    
+
     If list is given for region, it must be of same length as bamPath and
     bamPaths[i] will use regions[i].
-    
+
     When appropriate, the jobs will be run in parallel.
 
     modified from http://sourceforge.net/apps/mediawiki/samtools/index.php?title=SAM_protocol#Python_APIs_.28Pysam.29
@@ -49,7 +49,7 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
             it = samfile.fetch(region=region)
         else:
             it = samfile.fetch()
-    
+
         # calculate the end position and print out BED
         take = (0, 2, 3) # CIGAR operation (M/match, D/del, N/ref_skip)
         outfile = bedFile
@@ -67,9 +67,9 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
                 outfile.flush()
             else:
                 outfile.close()
-           
+
     # # Do some validation and set up # #
-    
+
     # convert all that are allowed to be lists into lists
     if type(bamPath) == type(''):
         bamPath = [bamPath]
@@ -80,32 +80,36 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
             region = [region]
     else:
         region = [None]
-        
-    
+
+
     # enforce len(bamPathList) == len(bedFileList) unless len(bedFileList) == 1
     if (len(bedFile) != 1) and (len(bamPath) != len(bedFile)):
         raise ValueError('ERROR: If list given for bamPath AND bedFile, they must be of equal length,and bamPaths[i] is written to bedFile[i]. ')
-                
+
     # enforce len(bamPathList) == len(regionList) unless len(regionList) == 1
     if (len(region) != 1) and (len(bamPath) != len(region)):
-            raise ValueError('ERROR: If list is given for region, it must be of same length as bamPath andbamPaths[i] will use regions[i]. ')
-    
+        raise ValueError('ERROR: If list is given for region, it must be of same length as bamPath andbamPaths[i] will use regions[i]. ')
+
     ## enforce type(bedFile) == "<type 'file'> if only one bedFile given
     #if (len(bedFile) != 1) and (len(bamPath) != len(bedFile)):
         #raise TypeError('ERROR: If list given for bamPath AND NOT bedFile, bedFile must be an open file obj. ')
-        
-        
+
+
     # enforce type(bedFile) == type([]) if only one bamPath given
     if (len(bamPath) == 1) and (len(bedFile) > 1):
         raise TypeError('ERROR: If single bamPath, bedFile must be a single bed path. ')
-    
-        
+
+
     # equalize list lengths
     if len(bedFile) == 1:
         bedFile = bedFile * len(bamPath)
     if len(region) == 1:
         region = region * len(bamPath)
-    
+
+    # open files
+    for i in range(len(bamPath)):
+        bedFile[i] = open(bedFile[i],'a')
+        
     # # Initialize job_server if pp is avail and run jobs # #
     try:
         # if pp: run in parallel
@@ -113,17 +117,17 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
         print "Running with %s cpus." % (job_server.get_ncpus())
         jobs = []
         results = []
-	#pdb.set_trace()
+        #pdb.set_trace()
         for i in range(len(bamPath)):
             jobs.append(job_server.submit(func=convertBAM,
-                                          args=(bamPath[i],open(bedFile[i],'a'),region[i]),
+                                          args=(bamPath[i],bedFile[i],region[i]),
                                           depfuncs=(),
                                           modules=('pysam',),
                                           callback=None,
                                           callbackargs=(),
                                           group='default',
                                           globals=None))
-	
+
         for i in range(len(bamPath)):
             results.append(jobs[i]())
 
@@ -134,7 +138,7 @@ def bam2bed(bamPath,bedFile,region=None,ncpus="autodetect"):
             print "Alert: pp module not found, not running parallel."
             for i in range(len(bamPath)):
                 convertBAM(bamPath[i],bedFile[i],region[i])
-        
+
 
 
 def genephred2refflat(genePhredPath,refFlatPath):
