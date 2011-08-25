69821344d8ba905e9f97aced335fb25a0953e7fc
gus <gus@vlad.(none)>
2011-08-24 20:12:14 -0700

    rSeq/scripts/readsPerMillionMapped.py: - finished a preliminary functional version

--- rSeq/scripts/readsPerMillionMapped.py
+++ rSeq/scripts/readsPerMillionMapped.py
@@ -17,7 +17,7 @@ def main():
     -- List of transcript/gene symbols
     -- Name of condition/sample
     Outputs:
-    -- table of transcript/gene symbols, condition/sample name, and read counts normalized to each million mapped reads."""
+    -- table of transcript/gene symbols, condition/sample name,original read count, and read counts normalized to each million mapped reads."""
     
     
     usage = """python %prog [options]"""
@@ -34,36 +34,52 @@ def main():
     parser.add_option('-b', type='string', default=False,
                       help="""Path to the bam file representing the aligned reads for the desired condition/sample. (default=%default)""")
     parser.add_option('-l', type='string', default="All",
-                      help="""List of transcript/gene symbols. (default=%default)""")
+                      help="""Quoted, comma-delim'd list of transcript/gene symbols. (default=%default)""")
 
 
-
-    
     
     (opts, args) = parser.parse_args()
     
     if len(sys.argv) == 1:
         parser.print_help()
         exit(1)
+    
+    if not opts.l == "All":
+        opts.l = opts.l.split(',')
+    else:
+        opts.l = ''
 
-    
-    
-    opts.dir = opts.dir.rstrip('/')+'/'
+    opts.tx_col = int(opts.tx_col)
+    opts.rd_col = int(opts.rd_col)
     
     # get million mapped reads
     milMapped = float(pysam.flagstat(opts.b)[3].split()[0])/1000000
+    #milMapped = 7.5 # for quick debugging to bybass the bam filtering
     
     # parse list of tx/gene symbols into dict for tracking successes
-    symbols = {}
+    ##symbols = {}
+    ##if not opts.l == "All":
+        ##for sym in opts.l:
+            ##symbols[sym] = []
+    
+        
     
     # open expression table into rows
     rows = csv.reader(open(opts.t),delimiter='\t')
     
     # for each tx/gene in expFile: output million mapped reads (MMR)
-    #   if row[opts.tx-col].startswith(<any of the requested symbols>)
+    #   if row[opts.tx_col].startswith(<any of the requested symbols>)
     #   and update symbols with True if a hit is found.
+    print "Tx_symbol\tCondition\tOriginal_reads\tReads_per_million_mapped"
     for row in rows:
-        pass
+        if row[opts.tx_col].startswith(tuple(opts.l)):
+            try:
+                print "%s\t%s\t%s\t%s" % (row[opts.tx_col],
+                                      opts.c,
+                                      row[opts.rd_col],
+                                      float(row[opts.rd_col])/milMapped)
+            except:
+                print "failed:  %s" % (';'.join(row))
     
     # report whether/which requested symbols were not found or had errors??
 
