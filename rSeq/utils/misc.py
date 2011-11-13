60f21a1f6bf92647368f3ef3cdd626379af5ba0b
Augustine Dunn <wadunn83@gmail.com>
2011-11-13 13:59:19 -0800

    rSeq/utils/misc.py:  - added slidingWindow

--- rSeq/utils/misc.py
+++ rSeq/utils/misc.py
@@ -94,3 +94,26 @@ def pVal4mari(tabPath,tTx,tTxDN,tTxUP):
 def whoami():
     """Returns the name of the currently active function."""
     return sys._getframe(1).f_code.co_name
+
+def slidingWindow(sequence,winSize,step=1):
+    """Returns a generator that will iterate through
+    the defined chunks of input sequence.  Input sequence
+    must be iterable."""
+    
+    # Verify the inputs
+    try: it = iter(sequence)
+    except TypeError:
+        raise Exception("**ERROR** sequence must be iterable.")
+    if not ((type(winSize) == type(0)) and (type(step) == type(0))):
+        raise Exception("**ERROR** type(winSize) and type(step) must be int.")
+    if step > winSize:
+        raise Exception("**ERROR** step must not be larger than winSize.")
+    if winSize > len(sequence):
+        raise Exception("**ERROR** winSize must not be larger than sequence length.")
+    
+    # Pre-compute number of chunks to emit
+    numOfChunks = ((len(sequence)-winSize)/step)+1
+    
+    # Do the work
+    for i in range(0,numOfChunks,step):
+        yield sequence[i:i+winSize]
