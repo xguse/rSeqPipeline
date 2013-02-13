4e826da7bb0efced52a107f1c5036eafee628acb
Augustine Dunn <wadunn83@gmail.com>
2013-02-12 16:50:00 -0800

    rSeq/utils/errors.py: - updated error Base class and other to match gfunc

--- rSeq/utils/errors.py
+++ rSeq/utils/errors.py
@@ -1,23 +1,61 @@
-class SanityCheckError(StandardError):
+"""
+####################
+errors.py
+####################
+Code defining custom base error classes to provide a foundation for graceful error handling.
+"""
+import warnings
+
+class rSeqError(StandardError):
+    """Base class for exceptions in the rSeq package."""
+    pass
+
+
+
+
+class SystemCallError(rSeqError):
+    """Error raised when a problem occurs while attempting to run an external system call.
+
+    Attributes:
+        | ``errno`` -- return code from system call
+        | ``filename`` -- file in volved if any
+        | ``strerror`` -- error msg """
+    
+    def __init__(self,errno,strerror,filename=None):
+        self.errno = errno
+        self.strerror = strerror
+        self.filename = filename
+        
+    def __str__(self):
+        if not self.filename: 
+            return """ERROR:\n %s.\nRETURN_STATE: %s.""" % (self.strerror.strip('\n'),
+                                                          self.errno)
+        else: 
+            return """ERROR in %s:\n %s.\nRETURN_STATE: %s.""" % (self.filename,
+                                                                self.strerror.strip('\n'),
+                                                                self.errno)
+
+
+class SanityCheckError(rSeqError):
     """When values that "should" not be possible happen; like if a variable was changed unexpectedly."""
     pass
 
-class UnexpectedValueError(StandardError):
+class UnexpectedValueError(rSeqError):
     """When values that "should" not be possible happen; like if a variable was changed unexpectedly."""
     pass
 
-class InvalidFileFormatError(StandardError):
+class InvalidFileFormatError(rSeqError):
     """When errors occur due to malformed file formats."""
     pass
 
-class MissingArgumentError(StandardError):
+class MissingArgumentError(rSeqError):
     """When a required argument is missing from the parsed command line options."""
     def __init__(self,errMsg):
         self.msg = errMsg
     def __str__(self):
         return """ERROR: %s""" % (self.msg)
 
-class InvalidOptionError(StandardError):
+class InvalidOptionError(rSeqError):
     def __init__(self,optVal,optName,validVals=None):
         self.optVal    = optVal
         self.optName   = optName
@@ -29,16 +67,11 @@ class InvalidOptionError(StandardError):
         else:
             return """ERROR: %s is not a valid value for arg:%s.""" % (self.optVal,self.optName)
 
-class ExternalError(EnvironmentError):
-    """Exception raised when a problem occurs while attempting to run an external system call."""
-    def __str__(self):
-        if not self.filename: 
-            return """ERROR: %s.\nRETURN_STATE: %s.""" % (self.strerror.strip('\n'),
-                                                          self.errno)
-        else: 
-            return """ERROR in %s: %s.\nRETURN_STATE: %s.""" % (self.filename,
-                                                                self.strerror.strip('\n'),
-                                                                self.errno)
+class ExternalError(SystemCallError):
+    """Dummy class pointing to SystemCallError for backward compatability"""
+    
+    warnings.warn('You should be calling "SystemCallError" instead!',DeprecationWarning)
+
 
     
     
